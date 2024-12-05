024d078253342364716b3102850797822d46f71d
unknown <manojrawat87634@gmail.com>
2024-12-05 08:56:17 +0530

    inital commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,10 @@ import connectionToDatabase from "./db/connection.js";
 const app = express();
 
 app.listen(3000, async ()=>{
-    await connectionToDatabase();
-    console.log("Port is running on the servcer 3000");
+    try {
+        await connectionToDatabase();
+        console.log("Port is running on the servcer 3000");
+    } catch (error) {
+        console.log(error);
+    }
 });
