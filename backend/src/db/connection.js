e7d7a9e021b44c97b5fd7165d34bfbbb41b92755
unknown <manojrawat87634@gmail.com>
2024-12-09 14:04:22 +0530

    inital commit

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -6,7 +6,8 @@ const connectionToDatabase = async() => {
         await mongoose.connect('mongodb://0.0.0.0:27017');
         console.log('Connected Successfully');
     } catch (error) {
-        throw new Error("Some Error Occured");
+        console.log(error);
+        return;
     }
 }
 
