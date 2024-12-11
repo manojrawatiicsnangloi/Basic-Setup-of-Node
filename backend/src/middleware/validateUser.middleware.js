aa3dfafb762d2b43c0d28f8362ead6fc8ee92c05
unknown <manojrawat87634@gmail.com>
2024-12-11 16:59:45 +0530

    version-1.0

---
+++ backend/src/middleware/validateUser.middleware.js
@@ -0,0 +1,15 @@
+const validateUser = (schema) => async (req, res, next) => {
+    try {
+        schema.validate({
+            body: req.body,
+            params: req.params,
+            query: req.query
+        });
+        return next();
+    } catch (error) {
+        console.log(error);
+        res.json(error.errors).status(400);
+    }
+}
+
+export default validateUser;
